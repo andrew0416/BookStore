249fb32be055f15340f1e1b3c2d7008e6082b8fd
CreateWater <blackrp001@naver.com>
2025-03-13 17:56:01 +0900

    login 구현

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var join = require('../controller/UserController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/join', join);
 
 //join
 //login
